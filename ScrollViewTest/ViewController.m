359018545b904a6df836481e49dec53b8dcaff2f
Allen Snook <allen@allendav.com>
2014-10-06 15:24:18 -0700

    Create scrollview as root view properly

--- ScrollViewTest/ViewController.m
+++ ScrollViewTest/ViewController.m
@@ -10,8 +10,7 @@
 
 @interface ViewController ()
 
-@property (strong, nonatomic) UIScrollView *scrollView;
-@property (strong, nonatomic) UIView *redView;
+@property (strong, nonatomic) UITextView *redView;
 @property (strong, nonatomic) UIView *blueView;
 @property (strong, nonatomic) UIView *greenView;
 
@@ -21,51 +20,40 @@
 
 - (void)loadView
 {
-    [super loadView];
+    // Note: since we are constructing self.view, we do not want to call [super loadView]
     self.title = @"Wut?";
     
-    self.scrollView = [[UIScrollView alloc] init];
-    self.redView = [[UIView alloc] init];
+    // Create the root view as a scroll view
+    self.view = [[UIScrollView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame]];
+    self.redView = [[UITextView alloc] init];
+    self.redView.text = @"First line\nSecond line\nThird line\nFourth line";
     self.blueView = [[UIView alloc] init];
     self.greenView = [[UIView alloc] init];
 
-    self.scrollView.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0.3 alpha:1];
-    self.scrollView.translatesAutoresizingMaskIntoConstraints = NO; // we're going to define them below 
+    self.view.backgroundColor = [UIColor colorWithRed:0 green:0 blue:0.3 alpha:1];
 
     // New to iOS7 - YES is the default - to let the scrollview content start initially below the nav bar
     // self.automaticallyAdjustsScrollViewInsets = YES;
-    [self.view addSubview:self.scrollView];
     
     // Since we turned off translating the auto resizing mask into constraints, we need to add them
-    NSDictionary *views = @{@"scrollview": self.scrollView,
-                            @"redview": self.redView,
+    NSDictionary *views = @{@"redview": self.redView,
                             @"blueview": self.blueView,
                             @"greenview": self.greenView
                             };
-    
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[scrollview]|"
-                                                                      options:0
-                                                                      metrics:nil
-                                                                        views:views]];
-    
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[scrollview]|"
-                                                                      options:0
-                                                                      metrics:nil
-                                                                        views:views]];
 
     self.redView.backgroundColor = [UIColor colorWithRed:0.9 green:0.5 blue:0.5 alpha:0.8];
     self.redView.translatesAutoresizingMaskIntoConstraints = NO; // we're going to do it below
-    [self.scrollView addSubview:self.redView];
+    [self.view addSubview:self.redView];
     
     self.blueView.backgroundColor = [UIColor colorWithRed:0.5 green:0.5 blue:0.9 alpha:0.8];
     self.blueView.translatesAutoresizingMaskIntoConstraints = NO; // we're going to do it below
-    [self.scrollView addSubview:self.blueView];
+    [self.view addSubview:self.blueView];
     
     self.greenView.backgroundColor = [UIColor colorWithRed:0.5 green:0.9 blue:0.5 alpha:0.8];
     self.greenView.translatesAutoresizingMaskIntoConstraints = NO; // we're going to do it below
-    [self.scrollView addSubview:self.greenView];
+    [self.view addSubview:self.greenView];
     
-    // Pin the redView width to the container (superview) width (not the scroll view)
+    // Pin the redView width to the root view width
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.redView 
                                                           attribute:NSLayoutAttributeWidth
                                                           relatedBy:NSLayoutRelationEqual
@@ -74,7 +62,7 @@
                                                          multiplier:1.0f
                                                            constant:0]];
 
-    // Pin the blueView width to the container (superview) width (not the scroll view)
+    // Pin the blueView width to the root view width
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.blueView 
                                                           attribute:NSLayoutAttributeWidth
                                                           relatedBy:NSLayoutRelationEqual
@@ -83,7 +71,7 @@
                                                          multiplier:1.0f
                                                            constant:0]];
 
-    // Pin the greenView width to the container (superview) width (not the scroll view)
+    // Pin the greenView width to the root view width
     [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.greenView 
                                                           attribute:NSLayoutAttributeWidth
                                                           relatedBy:NSLayoutRelationEqual
